f1885fafd015afe5ecf46f79391a9002b6af7a45
James Perkins <jamesperkins@hey.com>
2021-11-02 15:42:33 -0400

    Adding it as a Svelte component

---
+++ src/components/Comments.svelte
@@ -0,0 +1,15 @@
+<script>
+import { onMount } from 'svelte'
+onMount(() => {
+    const s = document.createElement('script')
+    const tag = document.getElementById('utterances')
+    s.setAttribute('repo',"perkinsjr/jamesperkins_dev_comments")
+    s.setAttribute('issue-term',"pathname")
+    s.setAttribute('label',"✨💬")
+    s.setAttribute('theme',"preferred-color-scheme")
+    s.setAttribute('crossorigin',"anonymous")
+    s.src = 'https://utteranc.es/client.js'
+    tag.parentNode.insertBefore(s, tag)
+})
+</script>
+<div id="utterances"></div>
